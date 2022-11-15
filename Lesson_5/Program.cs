a3a9c0ac84853af07d9c8b80e9bf1af8fa3852cf
Анвар <hah@bk.ru>
2022-11-15 17:08:14 +0300

    Лекция 5

---
+++ Lesson_5/Program.cs
@@ -0,0 +1,28 @@
+﻿// Поэлементное копирование массива
+
+Console.WriteLine("Введите размер:");
+int size = Convert.ToInt32(Console.ReadLine());
+
+int[] array = GetArray (size);
+Console.WriteLine(String.Join(", ", array));
+int[] copyArray = CopyArray (array);
+Console.WriteLine(String.Join(", ", copyArray));
+
+int[] GetArray(int size)
+{
+    int[] array1 = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array1[i] = new Random().Next(100);
+    }
+    return array1;
+}
+int[] CopyArray(int[] array)
+{
+    int[] result = new int[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        result[i] = array[i];
+    }
+    return result;
+}
